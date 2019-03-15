c350c6595afb62581d438ce1bfbd2ab402410ce4
Caroline Longnecker <cvrieous@gmail.com>
2019-03-14 22:33:22 -0500

    Fix SecretConstants header and include base c++ file

--- code/espRFID/SecretConstants.h
+++ code/espRFID/SecretConstants.h
@@ -6,10 +6,10 @@
 #ifndef SECRETCONSTANTS_H_
 #define SECRETCONSTANTS_H_
 
-extern const char* ssid = "asdf";
-extern const char* password = "asdf";
-extern const char* mdnsName = "Door";
+extern const char* ssid;
+extern const char* password;
+extern const char* mdnsName;
 
-extern const char* GScriptId = "asdf";
+extern const char* GScriptId;
 
 #endif
